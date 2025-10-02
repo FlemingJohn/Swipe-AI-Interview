f174ea050f62a7f7fd951b83b74e5b680e5ad05b
FLEMING <flemingfleming504@gmail.com>
2025-10-02 12:09:26 +0000

    give suggestion in this logo change ???? (_for element <logo>_)

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,24 +1,7 @@
-import { LucideProps, User, Bot, BrainCircuit, Upload, Send, Search, ChevronsUpDown, Loader2 } from 'lucide-react';
+import { LucideProps, User, Bot, BrainCircuit, Upload, Send, Search, ChevronsUpDown, Loader2, Layers } from 'lucide-react';
 
 export const Icons = {
-  logo: (props: LucideProps) => (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      {...props}
-    >
-      <path d="M12 12m-4 0a4 4 0 1 0 8 0a4 4 0 1 0-8 0" />
-      <path d="M16 8V6a2 2 0 0 0-2-2H8a2 2 0 0 0-2 2v2" />
-      <path d="M12 12v3a2 2 0 0 0 2 2h2a2 2 0 0 0 2-2v-3" />
-      <path d="M8 17a2 2 0 0 0 2 2h2" />
-      <path d="M8 12H6a2 2 0 0 0-2 2v2a2 2 0 0 0 2 2h2" />
-    </svg>
-  ),
+  logo: Layers,
   user: User,
   bot: Bot,
   brain: BrainCircuit,
